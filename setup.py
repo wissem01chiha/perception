a6139a0b1160c55252a52f19f7f982568979bbe4
wissem01chiha <chihawissem08@gmail.com>
2024-03-13 23:07:38 +0100

    update setup.py file

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
 # Inspiration: https://stackoverflow.com/a/7071358/6064135
-with open('project_name/_version.py', 'r') as version_file:
+with open('robotics-perception/_version.py', 'r') as version_file:
     version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
     if version_groups:
         version = version_groups.group(1)
@@ -31,12 +31,12 @@ DEV_REQUIREMENTS = [
 ]
 
 setuptools.setup(
-    name='PROJECT_NAME_URL',
+    name='ROBOTICS_PERCEPTION',
     version=version,
     description='Your project description here',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/USERNAME/PROJECT_NAME_URL',
+    url='https://github.com/wissem01chiha/robotics-perception',
     author='USERNAME',
     license='MIT',
     packages=setuptools.find_packages(
@@ -61,7 +61,7 @@ setuptools.setup(
     },
     entry_points={
         'console_scripts': [
-            'PROJECT_NAME_URL=project_name.my_module:main',
+            'ROBOTICS_PERCEPTION=project_name.my_module:main',
         ]
     },
     python_requires='>=3.8, <4',
